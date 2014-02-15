90b8f70f1feaed75d8144dd1785f3e8fb9365092
Louis Croce <ljc2154@columbia.edu>
2014-02-15 17:42:16 -0500

    Script now calculates normalized term frequency, idf, and uses them to compute a term score for each document

--- Project1/bingtest.py
+++ Project1/bingtest.py
@@ -1,6 +1,8 @@
 import urllib2
 import base64
 import json
+from collections import defaultdict		# to initialize dictionary values to 0
+from math import log # for log for idf
 
 query = raw_input()
 query = query.replace(" ", "+")
@@ -16,22 +18,75 @@ response = urllib2.urlopen(req)
 content = response.read()
 #content contains the xml/json response from Bing. 
 results = json.loads(content)
-
-relevant = []
+docs = results['d']['results']
 
 print "Bing Search Results"
 print "==================="
 
+# Intialize idf dictionary to 0 for all documents
+idf = defaultdict(list)
+
+# traverse results
 for i in xrange(10):
+	# Print the search result for the user
 	print "Result", i+1
-	print "Title: " + results['d']['results'][i]['Title']
-	print "Description: " + results['d']['results'][i]['Description']
-	print "Url: " + results['d']['results'][i]['Url']
+	print "Title: " + docs[i]['Title']
+	print "Description: " + docs[i]['Description']
+	print "Url: " + docs[i]['Url']
+	# Obtain relevance feedback
 	answer = "X"
 	while answer is not "Y" and answer is not "N":
 		print "\nIs this relevant? (Y/N)"
 		answer = raw_input()
 	if answer is "Y":
-		relevant.append(results['d']['results'][i])
+		docs[i]['relevant'] = 'Y'
+	else:
+		docs[i]['relevant'] = 'N'
 	print "\n==================================================\n"
-print relevant[3]['Url']
+
+	# Initialize all term scores to 0 for document
+	docs[i]['scores'] = defaultdict(int)
+
+	# Remove unnecessary characters
+	docs[i]['Description'] = docs[i]['Description'].replace('.', '')
+	docs[i]['Description'] = docs[i]['Description'].replace(',', '')
+	docs[i]['Description'] = docs[i]['Description'].replace('&', '')
+	# terms is the list of words in a document
+	terms = docs[i]['Description'].split()
+	# wordCt represents the number of words in a given document
+	wordCt = float(len(terms))
+
+	#for each term slurped, increment term's score for document
+	for term in terms:
+		# increment normalized term score
+		docs[i]['scores'][term] = (docs[i]['scores'][term] + 1)/wordCt
+		
+		# elongate list if necessary
+		if (len(idf[term]) is 0):
+			idf[term] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+		# mark idf for this document
+		idf[term][i] = 1
+
+# Calculate actual idfs
+for key in idf:
+	count = 0
+	for i in xrange(10):
+		count = count + idf[key][i]
+	idf[key][10] = count
+
+# Apply idf to document's term scores
+for i in xrange(10):
+	for key in docs[i]['scores']:
+		docs[i]['scores'][key] = docs[i]['scores'][key] * log(10./idf[key][10])
+		print docs[i]['scores'][key]
+
+# 2 dictionaries for relevant and nonrelevant
+# taken as sums
+# multiply by coefficients
+# merge into master dictionary: subtract relvant from nonrelevant
+# take 2 highest non original query scores from master
+# sort terms by master score
+# set that as new query
+	
+	
