ba008ebbc9d54f87787b23e99b2f7e2bbf0e89bf
Louis Croce <ljc2154@columbia.edu>
2014-04-22 19:16:51 -0400

    csvGenerator works

--- Project3/csvGenerator.py
+++ Project3/csvGenerator.py
@@ -2,6 +2,15 @@ import csv
 from collections import defaultdict		# to initialize dictionary values to 0
 import os
 
+os.system("rm -f newCSV.csv")
+
+with open('Multi_Agency_Permits.csv',"rb") as source:
+	rdr= csv.reader( source )
+	with open("newCSV.csv","wb") as result:
+		wtr= csv.writer( result )
+		for r in rdr:
+			wtr.writerow( (r[4], r[16]) )
+
 os.system("rm -f INTEGRATED-DATASET.csv")
 
 source = open("newCSV.csv","rb")
@@ -9,7 +18,7 @@ rdr= csv.reader( source )
 
 blocks = defaultdict(dict)
 for row in rdr:
-	if row[1] != '0000000000' and row[0] != 'N/A' and row[0] != 'INCORRECT LICENSE':
+	if row[1] != '0000000000' and row[1] != '' and row[0] != 'N/A' and row[0] != 'INCORRECT LICENSE':
 		blocks[row[1]][row[0]] = 1
 
 output = open('INTEGRATED-DATASET.csv', 'w')
