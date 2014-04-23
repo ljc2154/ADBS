325a61e74c5b61c7f1feeedeb7ebabf101cf17a6
Louis Croce <ljc2154@columbia.edu>
2014-04-22 20:21:08 -0400

    working on apriori

--- Project3/associationRuleMining.py
+++ Project3/associationRuleMining.py
@@ -1,5 +1,7 @@
 import os.path # for isfile() function
 import csv # for csv reader
+import sys # to exit
+from sets import Set # to check subset
 
 # Make sure script utilized correctly with command line arguments
 usageStr = "Usage error: Format must be python associationRuleMining.py <INTEGRATED-DATASET.csv> <Minimum Support> <Minimum Confidence>\n"
@@ -19,14 +21,70 @@ try:
 	min_sup = float(sys.argv[2])
 except ValueError:
 	sys.exit(usageStr)
-if min_sup < 0 or min_sup > 1
+if min_sup < 0 or min_sup > 1:
 	sys.exit(usageStr)
 try:
 	min_conf = float(sys.argv[3])
 except ValueError:
 	sys.exit(usageStr)
-if min_conf < 0 or min_conf > 1
+if min_conf < 0 or min_conf > 1:
 	sys.exit(usageStr)
 
 
 # Compute Frequent Itemsets
+
+# Generate master list of all possible items
+masterItemset = []
+rows = 0
+csvfile = open(integrated_dataset, 'rb')
+datasetreader = csv.reader(csvfile, quotechar='|')
+for row in datasetreader:
+	rows += 1
+	for i in row:
+		if i not in masterItemset:
+			masterItemset.append(i)
+
+# A Priori Algorithm
+listofLs = []
+# set L_1
+newL = []
+listofLs.append(newL)
+for item in masterItemset:
+	tempList = []
+	tempList.append(item)
+	listofLs[0].append(tempList)
+
+while len(listofLs[-1]) > 0:
+	lastL = listofLs[-1]
+	# generate C_k
+	c = []
+	cCount = []
+	for itemSet in lastL:
+		for item in masterItemset:
+			newSet = itemSet
+			if item not in newSet:
+				newSet.append(item)
+				c.append(newSet)
+				cCount.append(0)
+
+	# traverse transactions (rows of csv)
+	for row in datasetreader:
+		for i in xrange(len(c)):
+			if Set(c[i]).issubset(Set(row)):
+				cCount[i] += 1
+	
+	# Generate L_k
+	newL = []
+	for i in xrange(len(c)):
+		if cCount[i]/rows > min_sup:
+			newL.append(c[i])
+
+	# Append L_k
+	listofLs.append(newL)
+
+# Create master list of itemsets
+masterL = []
+for l_k in listofLs:
+	for itemSet in l_k:
+		print itemSet
+		masterL.append(itemSet)
